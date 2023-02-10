286a8df24d5fea8dd8b625622d502b2f68a8db3c
NepalJohn21 <124006434+NepalJohn21@users.noreply.github.com>
2023-02-10 17:51:17 -0300

    awesome

---
+++ config.js
@@ -0,0 +1,7 @@
+// Don't Change this !
+var icon_doc = document.querySelector(".icon-bases")
+//Please default/custom on this icon:
+const icon = "" //Change this icon/image (small size) in external url or path
+
+// On the document:
+icon_doc.rel = icon // This page is loaded to change this icon. Dynamic Icon.
